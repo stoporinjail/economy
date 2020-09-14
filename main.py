460129ec0ea551460e2a6972b45512ad220e5256
Dinh Ho <ho465374@student.aisd.net>
2020-09-14 20:29:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+prefix = '!'
+import random as r
+money = 0
+def help():
+  print(f'''Commands:
+  -----------------------------------------------------------
+  {prefix}help - Displays help.
+  {prefix}prefix - Changes prefix
+  {prefix}work - Get some money!
+  {prefix}crime - Get some money (sometimes a fee)
+  {prefix}bet - Bet!
+  {prefix}bal''')
+help()
+while True:
+  c = input('What is your command? ')
+  if c == prefix+'help':
+    help()
+  elif c == prefix+'prefix':
+    q = input('What to change to? ')
+    prefix = q
+  elif c == prefix+'work':
+    amount = r.randint(50,200)
+    print(f'You get ${amount} for working at Pizza Hut!')
+    money += amount
+  elif c == prefix+'crime':
+    outcome = ['good','bad']
+    r.shuffle(outcome)
+    if outcome[0] == 'good':
+      amount = r.randint(50,200)
+      print(f'You manage to steal ${amount}')
+      money += amount
+    else:
+      amount = r.randint(50,200)
+      print(f'You got caught, and pay ${amount}!')
+      money -= amount
+  elif c == prefix+'bet':
+    qq = input('How much? ')
+    qq = int(qq)
+    outcome = ['good','bad']
+    r.shuffle(outcome)
+    if outcome[0] == 'good':
+      qq += qq
+      print(f'You get ${qq}!')
+      money += qq
+    else:
+      print('You lose your money. :(')
+      money -= qq
+  elif c == 'bal':
+    print(f'Your balance is {money})
+    
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Install python
+[Python](python.org) (3.8.2)
+# Clone Repo
+```
+git clone https://github.com/
